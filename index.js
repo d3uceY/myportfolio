1c78bbd1bbabe4d90ebc90ca8992c8b3e42002ff
d3uceY <onyekwelujesse1234@gmail.com>
2023-08-22 08:36:09 -0700

    i have made chnages to this repository

--- index.js
+++ index.js
@@ -1,48 +1,3 @@
-// typing effect
-
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
-
-const textArray = ["A Frontend Developer", "A Freelancer"];
-const typingDelay = 100;
-const erasingDelay = 50;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  }
-  else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
-  if (textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
-// typing effect
 
 
 //easter egg
@@ -130,13 +85,6 @@ gsap.to('#logo-overlay', {
   duration: .3,
   delay: 2.5,
   ease: "expo.out"
-})
-
-gsap.to('#logo-overlay-white', {
-  width: 0,
-  duration: .4,
-  delay: 3.2,
-  ease: "power3.out"
 })
 
 
